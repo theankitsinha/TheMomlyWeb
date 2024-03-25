66eea22e615290960792db34208d30857a7bc8b7
Ankit Sinha <ankitsinhaofficial1@gmail.com>
2024-03-26 00:49:15 +0530

    - Updated Prisma Accelerate

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,5 @@
 import {PrismaClient} from '@prisma/client'
+import {withAccelerate} from '@prisma/extension-accelerate'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 //@ts-ignore
@@ -10,7 +11,7 @@ export const prisma =
     globalForPrisma.prisma ||
     new PrismaClient({
         log: ['query'],
-    })
+    }).$extends(withAccelerate())
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
