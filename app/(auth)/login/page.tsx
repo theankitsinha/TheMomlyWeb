f3efe0aff7e5c152f0c57b6f4181793d0567521c
Ankit Sinha <ankitsinhaofficial1@gmail.com>
2024-03-26 00:50:52 +0530

    - Updated Login Momly Page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,6 @@
-import {Metadata} from 'next';
 import React from 'react';
 import ComponentsAuthLoginForm from "@/components/auth/components-auth-login-form";
 
-export const metadata: Metadata = {
-    title: 'Login',
-};
 
 const CoverLogin = () => {
     return (
